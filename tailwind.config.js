8ad56ec095286cdad2f2616e48d9e7f327777a02
Abayomi Aishida <yomicorperate@gmail.com>
2023-07-29 10:20:28 +0100

    Footer, Pricing and Contact pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.html", "./layout/**/*.js"],
+  content: ["./**/*.html", "./blog/**/*.[html,js]"],
   theme: {
     extend: {},
   },
